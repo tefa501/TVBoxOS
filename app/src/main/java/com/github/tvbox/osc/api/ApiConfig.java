a5e0b2a24e60e5037c77d962011850d95617cedb
tefa501 <155060291+tefa501@users.noreply.github.com>
2023-12-29 11:16:47 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://xn--sss604efuw.top/tv");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
