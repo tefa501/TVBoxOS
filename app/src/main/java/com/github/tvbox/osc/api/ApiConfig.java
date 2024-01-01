8786b3113e9872c07b0e46a8b3791760221ead2f
tefa501 <155060291+tefa501@users.noreply.github.com>
2024-01-01 22:06:13 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://xn--sss604efuw.top/tv");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://mytv.tefa501.top:40296/tv/道长线路.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
